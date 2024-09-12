f3a23a05361c994d09a6f09936c42039d27f3aaf
Kvemzi <93746346+Kvemzi@users.noreply.github.com>
2024-09-13 00:49:30 +0200

    Delete petrecords directory
    New directory incoming

--- petrecords/pets/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import Pet, VetService
-
-admin.site.register(Pet)
-admin.site.register(VetService)
--- petrecords/pets/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-def pet_list(request):
-    # Logika za prikaz liste kućnih ljubimaca
-    pass
-
-def vet_service_list(request, pet_id):
-    # Logika za prikaz liste veterinarskih usluga za određenog kućnog ljubimca
-    pass
